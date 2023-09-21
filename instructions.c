beef7af4e6f7f1d9a4a53672bc062e39abb5b3d3
0ussamabernou <oussama_bernou@um5.ac.ma>
2023-09-21 19:45:43 +0100

    update

--- instructions.c
+++ instructions.c
@@ -5,12 +5,9 @@ void run_instuction(char *opcode, char *value, uint line_num)
 	uint i;
 
 	instruction_t instructions[] = {
-	    {"push", push},	 {"pall", print_stack}, {"pint", print_top},
-	    {"pop", pop},	 {"nop", nop},		{"swap", swap_nodes},
-	    {"add", add_nodes},	 {"sub", sub_nodes},	{"div", div_nodes},
-	    {"mul", mul_nodes},	 {"mod", mod_nodes},	{"pchar", print_char},
-	    {"pstr", print_str}, {"rotl", rotl},	{"rotr", rotr},
-	    {NULL, NULL}};
+	    {"push", push},	{"pall", print_stack}, {"pint", print_top},
+	    {"pop", pop},	{"nop", nop},	       {"swap", swap_nodes},
+	    {"add", add_nodes}, {NULL, NULL}};
 
 	if (opcode[0] == '#')
 		return;
@@ -23,6 +20,7 @@ void run_instuction(char *opcode, char *value, uint line_num)
 			return;
 		}
 	}
+
 	invalid_op_err(line_num, opcode);
 }
 void exec(void *func, char *value, uint line_num) { return; }
